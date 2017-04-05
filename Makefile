f7a1012401a324e4dc558f1ecc5363a54520e6f1
Gustavo Zambonin <gzmbnn@gmail.com>
2017-04-05 20:03:45 -0300

    sudoku: possibly using threads * printing wrong output

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-CFLAGS = -Wall -Wextra -march=native -pipe -Os -s
+CFLAGS = -Wall -Wextra -march=native -pipe -lpthread
 
 all: build
 
 build: $(basename $(wildcard *.c))
+
+debug: CFLAGS += -g
+debug: build
 
 test: build
 	@for f in test/*_grid_* ; do echo $$f ; ./sudoku_verifier $$f ; done | less
--- sudoku_verifier.c
+++ sudoku_verifier.c
@@ -1,54 +1,132 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define SIZE  9
+#define THREAD_ID       syscall(__NR_gettid)
 #define hamming_comp(x)  __builtin_popcount(~x & 0x3ff)
 
-short error_count = 0;
+
+typedef void (*func_ptr)(short);
+typedef struct { func_ptr check; short param; } task;
+
+static short error_count = 0;
+static unsigned short grid[SIZE][SIZE];
+static pthread_t *threads;
+static pthread_mutex_t scheduler, function;
+static task tasks[SIZE * 3];
+static int available_tasks = (1 << 27) - 1;
+
+void load_grid(unsigned short[][SIZE], char*);
+void check_row(short);
+void check_col(short);
+void check_sqr(short);
+void* choose_task();
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+    printf("Usage: %s input_grid nthreads\n", argv[0]);
+		exit(1);
+	}
+
+	load_grid(grid, argv[1]);
+
+  int nthreads = atoi(argv[2]);
+  threads = calloc((unsigned int) nthreads, sizeof(pthread_t));
+
+  printf("Quebra-cabecas fornecido:\n");
+  for (int i = 0; i < SIZE; ++i) {
+    for (int j = 0; j < SIZE; ++j) {
+      printf("%hu ", grid[i][j]);
+    }
+    printf("\n");
+  }
+
+  for (short i = 0; i < SIZE; ++i) {
+    tasks[i].check = check_row;
+    tasks[i].param = i % 9;
+  }
+
+  for (short i = SIZE; i < SIZE * 2; ++i) {
+    tasks[i].check = check_col;
+    tasks[i].param = i % 9;
+  }
+
+  for (short i = SIZE * 2; i < SIZE * 3; ++i) {
+    tasks[i].check = check_sqr;
+    tasks[i].param = i % 9;
+  }
+
+  pthread_mutex_init(&scheduler, NULL);
+  pthread_mutex_init(&function, NULL);
+  for (int i = 0; i < nthreads; ++i) {
+    pthread_create(&threads[i], NULL, choose_task, NULL);
+  }
+  pthread_mutex_destroy(&function);
+  pthread_mutex_destroy(&scheduler);
+
+  for (int i = 0; i < nthreads; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  printf("Erros encontrados: %d.\n", error_count);
+
+  free(threads);
+
+  return EXIT_SUCCESS;
+}
 
 void load_grid(unsigned short grid[][SIZE], char* file) {
-
 	FILE* input = fopen(file, "r");
 
   if (input == NULL) {
-    return;
+    exit(1);
   }
 
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
+  for (int i = 0; i < SIZE; ++i) {
+    for (int j = 0; j < SIZE; ++j) {
       fscanf(input, "%hu", &grid[i][j]);
     }
   }
 
   fclose(input);
-
 }
 
-void check_row(unsigned short grid[][SIZE], short row_index) {
-
+void check_row(short row_index) {
   unsigned short repeated = 1;
 
   for (short i = 0; i < SIZE; ++i) {
     repeated |= 1 << grid[row_index][i];
   }
 
-  error_count += hamming_comp(repeated);
-
+  pthread_mutex_lock(&function);
+  short errors = hamming_comp(repeated);
+  if (errors) {
+    printf("Thread %ld: erro na linha %d.\n", THREAD_ID, row_index);
+    error_count += errors;
+  }
+  pthread_mutex_unlock(&function);
 }
 
-void check_col(unsigned short grid[][SIZE], short col_index) {
-
+void check_col(short col_index) {
   unsigned short repeated = 1;
 
   for (short i = 0; i < SIZE; ++i) {
     repeated |= 1 << grid[i][col_index];
   }
 
-  error_count += hamming_comp(repeated);
-
+  pthread_mutex_lock(&function);
+  short errors = hamming_comp(repeated);
+  if (errors) {
+    printf("Thread %ld: erro na coluna %d.\n", THREAD_ID, col_index);
+    error_count += errors;
+  }
+  pthread_mutex_unlock(&function);
 }
 
-void check_sqr(unsigned short grid[][SIZE], short sqr_index) {
-
+void check_sqr(short sqr_index) {
   short r = sqr_index / 3 * 3, c = sqr_index % 3 * 3, repeated = 1;
 
   for (short i = r; i < r + 3; ++i) {
@@ -57,33 +135,24 @@ void check_sqr(unsigned short grid[][SIZE], short sqr_index) {
     }
   }
 
-  error_count += hamming_comp(repeated);
-
+  pthread_mutex_lock(&function);
+  short errors = hamming_comp(repeated);
+  if (errors) {
+    printf("Thread %ld: erro na regiao %d.\n", THREAD_ID, sqr_index);
+    error_count += errors;
+  }
+  pthread_mutex_unlock(&function);
 }
 
-int main(int argc, char *argv[]) {
-
-	if (argc != 2) {
-    printf("Usage: %s input_grid\n", argv[0]);
-		return 1;
-	}
-
-  unsigned short grid[SIZE][SIZE];
-	load_grid(grid, argv[1]);
-
-  printf("Quebra-cabecas fornecido:\n");
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
-      printf("%hu ", grid[i][j]);
+void* choose_task() {
+  for (short i = 0; i < SIZE * 3; ++i) {
+    pthread_mutex_lock(&scheduler);
+    if ((available_tasks >> i) & 1) {
+      tasks[i].check(tasks->param);
+      available_tasks &= ~(1 << i);
     }
-    printf("\n");
-    check_row(grid, i);
-    check_col(grid, i);
-    check_sqr(grid, i);
+    pthread_mutex_unlock(&scheduler);
   }
 
-  printf("Erros encontrados: %d.\n", error_count);
-
-	return 0;
-
+  return NULL;
 }
