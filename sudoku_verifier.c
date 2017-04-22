a7185455d631484df41c3a2df152348fddc0179b
Gustavo Zambonin <gzmbnn@gmail.com>
2017-04-22 18:24:46 -0300

    sudoku: larger maximum number of threads * threads are now 1-indexed

--- sudoku_verifier.c
+++ sudoku_verifier.c
@@ -7,16 +7,18 @@
 #define length(x)   (sizeof(x) / sizeof((x)[0]))
 
 void load_grid(uint8_t /*grid*/[][SIZE], char* /*file*/),
-     print_errors(uint16_t /*count*/, uint8_t /*index*/, char* /*type*/),
-     check_row(uint8_t /*row_index*/), check_col(uint8_t /*col_index*/),
-     check_sqr(uint8_t /*sqr_index*/);
+     print_errors(uint16_t /*count*/, uint8_t /*index*/,
+                  uint32_t /*tid*/, char* /*type*/),
+     check_row(uint8_t /*row_index*/, uint32_t /*tid*/),
+     check_col(uint8_t /*col_index*/, uint32_t /*tid*/),
+     check_sqr(uint8_t /*sqr_index*/, uint32_t /*tid*/);
 void* choose_task(void* /*len*/);
 
 static uint8_t error_count = 0, grid[SIZE][SIZE];
 static pthread_t *threads;
 static pthread_mutex_t buf_mutex, err_mutex;
 
-typedef void (*func_ptr_t)(uint8_t);
+typedef void (*func_ptr_t)(uint8_t, uint32_t);
 static const func_ptr_t tasks[] = { [0 ... SIZE - 1] = check_row,
   [SIZE ... SIZE * 2 - 1] = check_col,
   [SIZE * 2 ... SIZE * 3 - 1] = check_sqr };
@@ -29,17 +31,17 @@ int32_t main(int32_t argc, char **argv) {
   }
 
   load_grid(grid, argv[1]);
-  uint64_t nthreads = strtoul(argv[2], NULL, 10);
+  uint16_t nthreads = strtoul(argv[2], NULL, 10);
   threads = calloc(nthreads, sizeof(pthread_t));
 
   pthread_mutex_init(&buf_mutex, NULL);
   pthread_mutex_init(&err_mutex, NULL);
 
-  for (uint8_t i = 0; i < nthreads; ++i) {
-    pthread_create(&threads[i], NULL, choose_task, (void*) length(tasks));
+  for (uint16_t i = 0; i < nthreads; ++i) {
+    pthread_create(&threads[i], NULL, choose_task, (void*) (intptr_t) (i + 1));
   }
 
-  for (uint8_t i = 0; i < nthreads; ++i) {
+  for (uint16_t i = 0; i < nthreads; ++i) {
     pthread_join(threads[i], NULL);
   }
 
@@ -70,25 +72,25 @@ void load_grid(uint8_t m[][SIZE], char* file) {
   fclose(input);
 }
 
-void check_row(uint8_t row_index) {
+void check_row(uint8_t row_index, uint32_t tid) {
   uint16_t repeated = 0;
   for (uint8_t i = 0; i < length(grid[0]); ++i) {
     repeated |= 1 << (grid[row_index][i] - 1);
   }
 
-  print_errors(repeated, row_index, "linha");
+  print_errors(repeated, row_index, tid, "linha");
 }
 
-void check_col(uint8_t col_index) {
+void check_col(uint8_t col_index, uint32_t tid) {
   uint16_t repeated = 0;
   for (uint8_t i = 0; i < length(grid[0]); ++i) {
     repeated |= 1 << (grid[i][col_index] - 1);
   }
 
-  print_errors(repeated, col_index, "coluna");
+  print_errors(repeated, col_index, tid, "coluna");
 }
 
-void check_sqr(uint8_t sqr_index) {
+void check_sqr(uint8_t sqr_index, uint32_t tid) {
   uint8_t r = sqr_index / 3 * 3, c = sqr_index % 3 * 3;
   uint16_t repeated = 0;
   for (uint8_t i = r; i < r + 3; ++i) {
@@ -97,26 +99,26 @@ void check_sqr(uint8_t sqr_index) {
     }
   }
 
-  print_errors(repeated, sqr_index, "regiao");
+  print_errors(repeated, sqr_index, tid, "regiao");
 }
 
-void print_errors(uint16_t count, uint8_t index, char* type) {
+void print_errors(uint16_t count, uint8_t index, uint32_t tid, char* type) {
   int32_t errors = __builtin_popcount(~count & ((1 << SIZE) - 1));
   if (errors) {
-    printf("Thread %lu: erro na %s %d.\n", pthread_self(), type, index + 1);
+    printf("Thread %d: erro na %s %d.\n", tid, type, index + 1);
     pthread_mutex_lock(&err_mutex);
     error_count += errors;
     pthread_mutex_unlock(&err_mutex);
   }
 }
 
-void* choose_task(void* len) {
-  for (uint8_t i = 0; i < (intptr_t) len; ++i) {
+void* choose_task(void* tid) {
+  for (uint8_t i = 0; i < length(tasks); ++i) {
     pthread_mutex_lock(&buf_mutex);
     if ((available_tasks >> i) & 1) {
       available_tasks &= ~(1 << i);
       pthread_mutex_unlock(&buf_mutex);
-      tasks[i](i % SIZE);
+      tasks[i](i % SIZE, (intptr_t) tid);
     } else {
       pthread_mutex_unlock(&buf_mutex);
     }
